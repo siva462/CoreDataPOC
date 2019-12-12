88dca67f8100adf4e481d64d61d634e48e67fd67
siva462 <puli.reddy3@wipro.com>
2019-12-12 15:54:02 +0530

    removed unwanted files

---
+++ CoredataPOC/Models/CustomUsers.swift
@@ -0,0 +1,25 @@
+//
+//  Users.swift
+//  CoredataPOC
+//
+//  Created by Wipro on 11/12/19.
+//  Copyright © 2019 wipro. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Users: NSObject {
+    var first_name = String()
+    var email = String()
+    var last_name = String()
+    
+    init(first_name: String, email: String, last_name: String)
+    {
+        self.first_name = first_name
+        self.email = email
+        self.last_name = last_name
+    }
+    
+}
+
